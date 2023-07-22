10f7092de14d0052f6da5986b8ae3d0e1be85ad8
GHOST-Aram <owinoojala@gmail.com>
2023-07-22 13:13:18 +0300

    Chore: Encapsulate 404 error handler functions

--- app.js
+++ app.js
@@ -2,27 +2,29 @@ import createHttpError from 'http-errors';
 import { router as indexRouter } from './catalog/routes/index.js';
 import { app } from './zghost/app/init.js';
 import { config } from './zghost/app/config.js';
+import { catchAndForward404Error, handle404Error } from './zghost/utils/errors.js';
 
 
 
 const createError = createHttpError
 
-// view engine setup
 config()
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
+
+
+
+
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(catchAndForward404Error);
+// error handler
+app.use(handle404Error);
 export {app};
---
+++ zghost/utils/errors.js
@@ -0,0 +1,13 @@
+export const catchAndForward404Error = function(req, res, next) {
+    next(createError(404));
+  }
+
+export const handle404Error = function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  }
