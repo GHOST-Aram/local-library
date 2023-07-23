a724d5c0aa1f05d23deb277c45e1f7c82600bfe0
GHOST-Aram <owinoojala@gmail.com>
2023-07-23 14:33:34 +0300

    chore: Restore app.js code as generated by express-generator

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-import createHttpError from 'http-errors';
 import { router as catalog } from './catalog/urls.js';
+import { cookie_parser } from './zghost/app/init.js';
+import { urlencoded } from './zghost/app/init.js';
+import { json } from './zghost/app/init.js';
+import { statics } from './zghost/app/init.js';
 import { app } from './zghost/app/init.js';
-import { config } from './zghost/app/config.js';
-import { catchAndForward404Error, handle404Error } from './zghost/utils/errors.js';
-
-
+import { logger } from './zghost/app/init.js';
+import createHttpError from 'http-errors';
 
 const createError = createHttpError
 
-config()
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookie_parser());
+app.use(statics('public'));
+
 app.all('/', (req, res) =>{
   res.redirect('/catalog')
 })
 app.use('/catalog', catalog);
 
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-
-
-
-
-
-
-
-
-
-
-
-// catch 404 and forward to error handler
-app.use(catchAndForward404Error);
 // error handler
-app.use(handle404Error);
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 export {app};
