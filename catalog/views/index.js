693240461a15a2aac7df51bc751a2c5a34b827d3
GHOST-Aram <owinoojala@gmail.com>
2023-07-24 11:54:44 +0300

    feat & chore: Dislplay number of book instances on homepage              -Refactor index view using Promise.all

--- catalog/views/index.js
+++ catalog/views/index.js
@@ -2,18 +2,28 @@ import { Author } from "../models/author.js";
 import { Genre } from '../models/genre.js'
 import { asynchHandler } from "../../zghost/app/init.js";
 import { Book } from "../models/book.js";
+import { BookInstance } from "../models/book-instance.js";
 
 
 export const index = asynchHandler(async(req, res) =>{
-    const numAuthors = await Author.countDocuments().exec()
-    const numGenres = await Genre.countDocuments().exec()
-    const numBooks = await Book.countDocuments().exec()
+    const [
+        numAuthors,
+        numBooks,
+        numGenres,
+        numBookInstances
+    ] = await Promise.all([
+        Author.countDocuments().exec(),
+        Book.countDocuments().exec(),
+        Genre.countDocuments().exec(),
+        BookInstance.countDocuments().exec()
+    ])
     
     const context = { 
         title: 'Home', 
         numAuthors, 
         numGenres,
-        numBooks 
+        numBooks,
+        numBookInstances 
     }
     res.render('catalog/index', context);
 })
