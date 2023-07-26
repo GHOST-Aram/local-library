12533f4448a44733bb7c8b6c24d463ea641c8290
GHOST-Aram <owinoojala@gmail.com>
2023-07-26 14:02:18 +0300

    feat: Display Number of genres on homepage
         -Get number of genres in index view

--- catalog/controllers/index.js
+++ catalog/controllers/index.js
@@ -1,11 +1,13 @@
 import { Author } from "../models/author.js";
+import { Genre } from '../models/genre.js'
+import { asynchHandler } from "../../zghost/app/init.js";
 
 
-export const index = (req, res) =>{
-    Author.countDocuments().then(
-        numAuthors => {
-            res.render('catalog/index', { title: 'Home', numAuthors });
-        }
-    )
-}
+export const index = asynchHandler(async(req, res) =>{
+    const numAuthors = await Author.countDocuments({}).exec()
+    const numGenres = await Genre.countDocuments({}).exec()
+    
+        
+    res.render('catalog/index', { title: 'Home', numAuthors, numGenres });
+})
 
