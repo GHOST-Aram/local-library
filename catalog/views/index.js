ce80c8019859387f6baaf1d66af3d8c63b2031a0
GHOST-Aram <owinoojala@gmail.com>
2023-07-27 17:34:45 +0300

    feat: Display number of books on libray home page

--- catalog/views/index.js
+++ catalog/views/index.js
@@ -1,13 +1,20 @@
 import { Author } from "../models/author.js";
 import { Genre } from '../models/genre.js'
 import { asynchHandler } from "../../zghost/app/init.js";
+import { Book } from "../models/book.js";
 
 
 export const index = asynchHandler(async(req, res) =>{
-    const numAuthors = await Author.countDocuments({}).exec()
-    const numGenres = await Genre.countDocuments({}).exec()
+    const numAuthors = await Author.countDocuments().exec()
+    const numGenres = await Genre.countDocuments().exec()
+    const numBooks = await Book.countDocuments().exec()
     
-        
-    res.render('catalog/index', { title: 'Home', numAuthors, numGenres });
+    const context = { 
+        title: 'Home', 
+        numAuthors, 
+        numGenres,
+        numBooks 
+    }
+    res.render('catalog/index', context);
 })
 
